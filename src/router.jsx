b8823b41f27e1c8484ca9a65d2c25da15f5ce0b6
NezyMZ <maiconnvr@hotmail.com>
2026-06-11 21:43:55 -0300

    feat: Implementação Login, Cadastro e Tela Inicial

---
+++ src/router.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route, Navigate } from "react-router";
+import { useAuth } from "./contexts/AuthContext";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Cadastro from "./pages/Cadastro";
+
+function RotaProtegida({ children }) {
+    const { estaAutenticado } = useAuth();
+
+    if (!estaAutenticado) return <Navigate to="/" replace />;
+
+    return children;
+}
+
+function AppRouter() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/home" element={<RotaProtegida><Home /></RotaProtegida>} />
+        </Routes>
+    );
+}
+
+export default AppRouter;
